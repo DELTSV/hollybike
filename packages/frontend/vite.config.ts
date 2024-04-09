cf4725cfe83ad8c7b31dec25ce2fbe8735095e68
Enzo Soares <enzoayrsoares@gmail.com>
2024-04-09 17:20:52 +0200

    Enabling react router dom

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,4 +4,10 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  }
 })
