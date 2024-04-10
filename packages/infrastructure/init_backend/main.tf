413b2b17d4be33ac15ca018dbbfed7cb79e7214c
Loïc Vanden Bossche <loloic066@gmail.com>
2024-04-10 10:48:27 +0200

    Added basic aws auth

---
+++ packages/infrastructure/init_backend/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+
+  backend "local" {
+    path = ".terraform.tfstate"
+  }
+}
+
+resource "aws_s3_bucket" "tfstate_bucket" {
+  bucket = "tfstate-bucket-323827"
+
+  tags = {
+    Name = "tfstate-bucket"
+    key = "tfstate"
+  }
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_access_key
+}
