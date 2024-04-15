3fce493d7be22b8155dd1b8c3fa3f0f84a6f0e98
Loïc Vanden Bossche <loloic066@gmail.com>
2024-04-15 23:15:36 +0200

    Try PR pipeline

--- .github/scripts/run-eagle-eye.sh
+++ .github/scripts/run-eagle-eye.sh
@@ -4,4 +4,4 @@ wget https://github.com/brainboard/eagle-eye/releases/download/v0.1.3/eagle-eye_
 
 chmod +x eagle-eye_Linux_x86_64
 
-./eagle-eye_Linux_x86_64 .
+./eagle-eye_Linux_x86_64 . --config ./eagle-eye-config.yml
