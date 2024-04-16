4e87a9c95d0d888a990b01f15abbe3741bda9f7f
Loïc Vanden Bossche <loloic066@gmail.com>
2024-04-16 19:37:31 +0200

    Removed completly private/public network separation

--- packages/infrastructure/project/network/vpc.tf
+++ packages/infrastructure/project/network/vpc.tf
@@ -28,39 +28,6 @@ data "aws_internet_gateway" "default" {
   }
 }
 
-
-resource "aws_eip" "nat_gateway" {
-  count = var.az_count
-
-  domain = "vpc"
-
-  tags = {
-    Name = "${var.namespace}_EIP_${count.index}_${var.environment}"
-  }
-}
-
-resource "aws_nat_gateway" "nat_gateway" {
-  count         = var.az_count
-  subnet_id     = aws_default_subnet.public[count.index].id
-  allocation_id = aws_eip.nat_gateway[count.index].id
-
-  tags = {
-    Name = "${var.namespace}_NATGateway_${count.index}_${var.environment}"
-  }
-}
-
-resource "aws_subnet" "private" {
-  count             = var.az_count
-  cidr_block = cidrsubnet(aws_default_vpc.default.cidr_block, 4, var.az_count + count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id            = aws_default_vpc.default.id
-
-  tags = {
-    Name = "${var.namespace}_PrivateSubnet_${count.index}_${var.environment}"
-  }
-}
-
-
 resource "aws_default_route_table" "default" {
   default_route_table_id = aws_default_vpc.default.main_route_table_id
 
@@ -72,37 +39,6 @@ resource "aws_default_route_table" "default" {
   tags = {
     Name = "${var.namespace}_PublicRouteTable_${var.environment}"
   }
-}
-
-resource "aws_route_table_association" "public" {
-  count          = var.az_count
-  subnet_id      = aws_default_subnet.public[count.index].id
-  route_table_id = aws_default_route_table.default.id
-}
-
-resource "aws_main_route_table_association" "public_main" {
-  vpc_id         = aws_default_vpc.default.id
-  route_table_id = aws_default_route_table.default.id
-}
-
-resource "aws_route_table" "private" {
-  count  = var.az_count
-  vpc_id = aws_default_vpc.default.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
-  }
-
-  tags = {
-    Name = "${var.namespace}_PrivateRouteTable_${count.index}_${var.environment}"
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  count          = var.az_count
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
 }
 
 resource "aws_vpc_endpoint" "secretsmanager_vpc_endpoint" {
