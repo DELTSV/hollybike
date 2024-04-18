7055a236d783b463c377adcf47b04810e5863d4e
Loïc Vanden Bossche <loloic066@gmail.com>
2024-04-18 13:30:46 +0200

    Added 1 subnet

--- packages/infrastructure/project/network/outputs.tf
+++ packages/infrastructure/project/network/outputs.tf
@@ -1,7 +1,7 @@
-# output list with only the first subnet to save costs
+# output list with only the first 2 subnets to save costs
 
 output "public_subnet_list" {
-  value = [aws_default_subnet.public[0].id]
+  value = slice(aws_default_subnet.public.*.id, 0, 2)
 }
 
 output "vpc_id" {
