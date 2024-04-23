be2cd206620f02c1b40614f833483edeb3339b16
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2024-04-23 22:21:51 +0200

    Added IPV6 secondary CIDR

--- packages/infrastructure/project/backend/load-balancers.tf
+++ packages/infrastructure/project/backend/load-balancers.tf
@@ -2,4 +2,6 @@ resource "aws_alb" "alb" {
   name            = "${var.namespace}-ALB-${var.environment}"
   security_groups = [aws_security_group.alb.id]
   subnets         = var.public_subnet_list
+
+  ip_address_type = "dualstack"
 }
