114ddff380ab7726f2d1fd0e0c8858123a6b89ce
Loïc Vanden Bossche <loloic066@gmail.com>
2024-04-26 19:21:23 +0200

    Full FTP file storage handling

--- packages/backend/src/main/kotlin/hollybike/api/services/storage/FTPStorageService.kt
+++ packages/backend/src/main/kotlin/hollybike/api/services/storage/FTPStorageService.kt
@@ -1,8 +1,10 @@
 package hollybike.api.services.storage
 
+import org.apache.commons.net.PrintCommandListener
 import org.apache.commons.net.ftp.FTP
 import org.apache.commons.net.ftp.FTPClient
 import java.io.ByteArrayInputStream
+import kotlin.concurrent.fixedRateTimer
 
 class FTPStorageService(
 	ftpServer: String?,
@@ -19,22 +21,61 @@ class FTPStorageService(
 			throw IllegalArgumentException("FTP configuration is missing")
 		}
 
+		ftpClient.addProtocolCommandListener(
+			PrintCommandListener(System.out, true)
+		)
+
 		ftpClient.connect(ftpServer)
-		ftpClient.login(ftpUsername, ftpPassword)
+
+		if (!ftpClient.login(ftpUsername, ftpPassword)) {
+			throw Exception("Cannot login to FTP server, verify your credentials")
+		}
+
 		ftpClient.enterLocalPassiveMode()
 		ftpClient.setFileType(FTP.BINARY_FILE_TYPE)
-		ftpClient.changeWorkingDirectory(ftpDirectory)
+
+		if (!ftpClient.changeWorkingDirectory(ftpDirectory)) {
+			throw Exception("The specified directory \"${ftpDirectory}\" does not exist")
+		}
+
+		fixedRateTimer("FTP keep alive", true, 0, 1000 * 60 * 5) {
+			ftpClient.sendNoOp()
+		}
+	}
+
+	private fun FTPClient.createDirectoryTree(directory: String) {
+		val directories = directory.split("/")
+
+		fun directoryExists(directoryPath: String, dir: String): Boolean {
+			val files = listDirectories(directoryPath)
+			return files.any { it.name == dir }
+		}
+
+		var currentPath = ""
+		for (dir in directories) {
+			if (!directoryExists(currentPath, dir)) {
+				val dirPath = currentPath + dir
+				makeDirectory(dirPath)
+			}
+
+			currentPath += "$dir/"
+		}
 	}
 
 	override suspend fun store(data: ByteArray, path: String, dataContentType: String) {
-		ftpClient.storeFile(path, ByteArrayInputStream(data))
+		val pathWithoutFile = path.substringBeforeLast("/")
+		ftpClient.createDirectoryTree(pathWithoutFile)
+
+		if (!ftpClient.storeFile(path, ByteArrayInputStream(data))) {
+			throw Exception("Cannot store file, check your permissions")
+		}
 	}
 
 	override suspend fun retrieve(path: String): ByteArray? {
-		// TODO: Retrieve the file from FTP
+		val outputStream = ftpClient.retrieveFileStream(path) ?: return null
+		val bytes = outputStream.readBytes()
 
-		println("Retrieving $path")
-
-		return null
+		ftpClient.completePendingCommand()
+		return bytes
 	}
 }
