7b635982d823583feec5aa52d277626b2c27f568
Enzo Soares <enzoayrsoares@gmail.com>
2024-05-10 01:19:55 +0200

    Removing analyze errors

--- packages/app/lib/auth/guards/auth_stream.dart
+++ packages/app/lib/auth/guards/auth_stream.dart
@@ -1,14 +1,10 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hollybike/auth/bloc/auth_bloc.dart';
 
 class AuthStream extends ChangeNotifier {
-  late final StreamSubscription<AuthState> _authStream;
-
   AuthStream(BuildContext context) {
-    _authStream = context.watch<AuthBloc>().stream.listen((event) {
+    context.watch<AuthBloc>().stream.listen((event) {
       notifyListeners();
     });
   }
