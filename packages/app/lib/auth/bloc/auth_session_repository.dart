ca51ed93f81d5389aa794c0fd84517ef31512a88
Enzo Soares <enzoayrsoares@gmail.com>
2024-05-10 01:19:55 +0200

    Removing analyze errors

--- packages/app/lib/auth/bloc/auth_session_repository.dart
+++ packages/app/lib/auth/bloc/auth_session_repository.dart
@@ -7,7 +7,6 @@ class AuthSessionRepository {
   const AuthSessionRepository({required this.authBloc});
 
   void sessionExpired(AuthSession session) {
-    print("expired session");
     authBloc.add(AuthSessionExpired(expiredSession: session));
   }
 }
