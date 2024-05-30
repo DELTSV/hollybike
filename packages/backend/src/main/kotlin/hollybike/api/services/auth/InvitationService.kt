b80fc8f3e2d27b74207b57d2e44ef4894cbaeb68
MacaronFR <denisft77@gmail.com>
2024-05-30 18:26:51 +0200

    Fix list information

--- packages/backend/src/main/kotlin/hollybike/api/services/auth/InvitationService.kt
+++ packages/backend/src/main/kotlin/hollybike/api/services/auth/InvitationService.kt
@@ -113,11 +113,13 @@ class InvitationService(
 					.innerJoin(Associations, { this.association }, { Associations.id })
 					.innerJoin(Users, { creator }, { Users.id })
 					.selectAll()
-					.applyParam(searchParam))
+					.applyParam(searchParam)
+					.andWhere {
+						Associations.id eq association.id
+					})
 				.with(Invitation::association)
 				.toList()
 		}
-		println(invitations)
 		return Result.success(invitations)
 	}
 
