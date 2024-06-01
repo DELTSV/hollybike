bd4d5ddd72dd897f3fc655b0248a03f027673526
MacaronFR <denisft77@gmail.com>
2024-06-01 15:16:47 +0200

    Better login

--- packages/frontend/src/auth/Login.tsx
+++ packages/frontend/src/auth/Login.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "./context.tsx";
 import { useNavigate } from "react-router-dom";
 import { Input } from "../components/Input/Input.tsx";
 import { Button } from "../components/Button/Button.tsx";
+import { Card } from "../components/Card/Card.tsx";
 
 export default function () {
 	const [email, setEmail] = useState("");
@@ -31,25 +32,30 @@ export default function () {
 	}, [auth.isLoggedIn]);
 
 	return (
-		<form className={"flex flex-col items-start"} onSubmit={e => e.preventDefault()}>
-			<Input
-				type={"email"}
-				placeholder="Email"
-				value={email}
-				onInput={e => setEmail(e.currentTarget.value)}
-			/>
-			<Input
-				type={"password"}
-				placeholder="Mot de passe" value={password}
-				onInput={e => setPassword(e.currentTarget.value)}
-			/>
-			<Button
-				onClick={() => {
-					login();
-				}}
-			>
-				Valider
-			</Button>
-		</form>
+		<div className={" w-full h-full flex justify-center items-center"}>
+			<form onSubmit={e => e.preventDefault()}>
+				<Card className={"flex flex-col items-center gap-4"}>
+					<p className={"text-2xl"}>Bienvenue sur l'interface de gestion d'Hollybike</p>
+					<Input
+						type={"email"}
+						placeholder="Email"
+						value={email}
+						onInput={e => setEmail(e.currentTarget.value)}
+					/>
+					<Input
+						type={"password"}
+						placeholder="Mot de passe" value={password}
+						onInput={e => setPassword(e.currentTarget.value)}
+					/>
+					<Button
+						onClick={() => {
+							login();
+						}}
+					>
+						Valider
+					</Button>
+				</Card>
+			</form>
+		</div>
 	);
 }
