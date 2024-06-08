0daa6071104fc916dd913c915e5a4f80daedf425
enzo soares <enzoayrsoares@gmail.com>
2024-06-08 07:51:04 +0200

    Adding top bar actions animations

--- packages/app/lib/shared/widgets/bar/top_bar_action_container.dart
+++ packages/app/lib/shared/widgets/bar/top_bar_action_container.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class TopBarActionContainer extends StatelessWidget {
+class TopBarActionContainer extends StatefulWidget {
   final void Function()? onPressed;
   final Widget? child;
 
@@ -11,23 +11,48 @@ class TopBarActionContainer extends StatelessWidget {
   });
 
   @override
+  State<TopBarActionContainer> createState() => _TopBarActionContainerState();
+}
+
+class _TopBarActionContainerState extends State<TopBarActionContainer>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
   Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.transparency,
-      child: Ink(
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Theme.of(context).colorScheme.primary,
-          border: Border.all(
-            width: 2,
-            color: Theme.of(context).colorScheme.onPrimary.withAlpha(100),
+    return ScaleTransition(
+      scale: _animation,
+      child: Material(
+        type: MaterialType.transparency,
+        child: Ink(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Theme.of(context).colorScheme.primary,
+            border: Border.all(
+              width: 2,
+              color: Theme.of(context).colorScheme.onPrimary.withAlpha(100),
+            ),
           ),
-        ),
-        child: InkWell(
-          onTap: onPressed,
-          child: child,
+          child: InkWell(
+            onTap: widget.onPressed,
+            child: widget.child,
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.bounceOut);
+
+    _controller.forward();
   }
 }
