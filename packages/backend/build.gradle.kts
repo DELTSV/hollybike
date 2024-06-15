f2be00cfe742be62f7b82550ea32e910555f077d
MacaronFR <denisft77@gmail.com>
2024-06-15 15:29:56 +0200

    Fix native build

--- packages/backend/build.gradle.kts
+++ packages/backend/build.gradle.kts
@@ -184,6 +184,12 @@ graalvmNative {
 
 			buildArgs.add("--initialize-at-run-time=de.nycode.bcrypt.BCryptKt")
 
+			buildArgs.add("--trace-class-initialization=sun.java2d.Disposer,sun.font.StrikeCache")
+			buildArgs.add("--initialize-at-run-time=sun.font.SunFontManager")
+			buildArgs.add("--initialize-at-run-time=sun.java2d.Disposer")
+			buildArgs.add("--initialize-at-run-time=sun.font.StrikeCache")
+			buildArgs.add("--initialize-at-run-time=sun.font.PhysicalStrike")
+
 			buildArgs.add("--initialize-at-run-time=org.simplejavamail.internal.util.MiscUtil")
 			buildArgs.add("--initialize-at-run-time=org.simplejavamail.internal.moduleloader.ModuleLoader")
 
