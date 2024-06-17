24c2426d7b7be3eddea47559f94e250e51110aa9
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2024-06-17 23:50:13 +0200

    Added empty map placeholder & get geojson from backend

--- packages/app/lib/event/fragments/details/event_details_map.dart
+++ packages/app/lib/event/fragments/details/event_details_map.dart
@@ -1,198 +1,37 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:geojson_vi/geojson_vi.dart';
 import 'package:hollybike/event/widgets/details/event_details_scroll_wrapper.dart';
-import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:hollybike/event/widgets/map/journey_map.dart';
+import 'package:hollybike/journey/type/minimal_journey.dart';
+import '../../widgets/map/empty_journey_map.dart';
 
-import '../../../shared/utils/waiter.dart';
-
-class EventDetailsMap extends StatefulWidget {
+class EventDetailsMap extends StatelessWidget {
   final int eventId;
+  final MinimalJourney? journey;
+  final void Function() onMapLoaded;
 
-  const EventDetailsMap({super.key, required this.eventId});
-
-  @override
-  State<EventDetailsMap> createState() => _EventDetailsMapState();
-}
-
-class _EventDetailsMapState extends State<EventDetailsMap> {
-  bool _mapLoading = true;
+  const EventDetailsMap({
+    super.key,
+    required this.eventId,
+    required this.journey,
+    required this.onMapLoaded,
+  });
 
   @override
   Widget build(BuildContext context) {
     return EventDetailsScrollWrapper(
-      scrollViewKey: 'event_details_map_${widget.eventId}',
-      child: SizedBox(
-    height: MediaQuery.of(context).size.height * 0.8,
-        child: Stack(
-          children: <Widget>[
-            MapWidget(
-              gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
-                Factory<OneSequenceGestureRecognizer>(
-                  () => EagerGestureRecognizer(),
-                ),
-              },
-              key: const ValueKey("mapWidget"),
-              onMapCreated: _onMapCreated,
-            ),
-            AnimatedOpacity(
-              opacity: _mapLoading ? 1 : 0,
-              duration: const Duration(milliseconds: 500),
-              child: IgnorePointer(
-                child: Container(
-                  color: Theme.of(context).scaffoldBackgroundColor,
-                  child: const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      scrollViewKey: 'event_details_map_$eventId',
+      child: _buildContent(),
     );
   }
 
-  void _onMapCreated(MapboxMap map) {
-    waitConcurrently(
-      map.loadStyleURI("mapbox://styles/mapbox/navigation-night-v1"),
-      rootBundle.loadString('assets/images/test.geojson'),
-    ).then((values) async {
-      final (_, geoJsonRaw) = values;
+  Widget _buildContent() {
+    if (journey == null) {
+      return const EmptyJourneyMap();
+    }
 
-      // map.style.addSource(
-      //   RasterDemSource(
-      //     id: 'mapbox-dem',
-      //     url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
-      //     tileSize: 512,
-      //   ),
-      // );
-      //
-      // map.style.addLayer(
-      //   HillshadeLayer(
-      //     id: 'hillshade-layer',
-      //     sourceId: 'mapbox-dem',
-      //     hillshadeExaggeration: 1.5,
-      //   ),
-      // );
-      //
-      // map.style.setStyleTerrain('{ "source": "mapbox-dem", "exaggeration": 1.5 }');
-
-      await Future.wait([
-        map.style.addSource(
-          GeoJsonSource(
-            id: 'tracks',
-            data: geoJsonRaw,
-          ),
-        ),
-        map.style.setLights(
-          AmbientLight(id: 'ambient-light', intensity: 0.5),
-          DirectionalLight(
-            castShadows: true,
-            shadowIntensity: 1,
-            id: 'directional-light',
-            intensity: 0.5,
-            color: 0XFFEC9F53,
-            direction: [0, 90],
-          ),
-        ),
-        map.style.addLayerAt(
-            LineLayer(
-              id: 'tracks-layer',
-              sourceId: 'tracks',
-              lineJoin: LineJoin.ROUND,
-              lineCap: LineCap.ROUND,
-              lineColor: 0xFF3457D5,
-              lineWidth: 5,
-              lineEmissiveStrength: 1,
-            ),
-            LayerPosition(
-              above: 'traffic-bridge-road-link-navigation',
-            )),
-        map.style.addLayerAt(
-          FillExtrusionLayer(
-            id: '3d-buildings',
-            sourceId: 'composite',
-            sourceLayer: 'building',
-            fillExtrusionOpacity: 0.8,
-            fillExtrusionColor: 0XFF515E72,
-          ),
-          LayerPosition(
-            above: 'tracks-layer',
-          ),
-        ),
-        map.style.setStyleLayerProperty(
-          "3d-buildings",
-          "fill-extrusion-height",
-          '[ "interpolate", ["linear"], ["zoom"], 15, 0, 15.05, ["get", "height"] ]',
-        ),
-        map.style.setStyleLayerProperty(
-          "3d-buildings",
-          "fill-extrusion-base",
-          '[ "interpolate", ["linear"], ["zoom"], 15, 0, 15.05, ["get", "min_height"] ]',
-        ),
-      ]);
-
-      final geoJson = GeoJSON.fromJSON(geoJsonRaw);
-
-      if (geoJson.bbox == null) return;
-
-      final bounds = CoordinateBounds(
-        southwest: Point(
-          coordinates: Position(
-            geoJson.bbox![0],
-            geoJson.bbox![1],
-          ),
-        ),
-        northeast: Point(
-          coordinates: Position(
-            geoJson.bbox![2],
-            geoJson.bbox![3],
-          ),
-        ),
-        infiniteBounds: false,
-      );
-
-      final cameraOptions = await map.cameraForCoordinateBounds(
-        bounds,
-        MbxEdgeInsets(
-          top: 25,
-          left: 50,
-          right: 50,
-          bottom: 75,
-        ),
-        null,
-        30,
-        null,
-        null,
-      );
-
-      final cameraBounds = await map.coordinateBoundsForCamera(cameraOptions);
-
-      await map.setBounds(
-        CameraBoundsOptions(
-          bounds: cameraBounds,
-        ),
-      );
-
-      await map.setCamera(cameraOptions);
-
-      setState(() {
-        _mapLoading = false;
-      });
-
-      await map.easeTo(
-          CameraOptions(
-            center: cameraOptions.center,
-            zoom: cameraOptions.zoom! + 0.3,
-            bearing: cameraOptions.bearing,
-            pitch: cameraOptions.pitch,
-          ),
-          MapAnimationOptions(
-            duration: 600,
-          ));
-    });
+    return JourneyMap(
+      journey: journey!,
+      onMapLoaded: onMapLoaded,
+    );
   }
 }
---
+++ packages/app/lib/event/widgets/map/empty_journey_map.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class EmptyJourneyMap extends StatelessWidget {
+  const EmptyJourneyMap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(56),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 150,
+              child: Lottie.asset(
+                'assets/lottie/lottie_journey.json',
+                repeat: false,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Aucun trajet n\'est renseigné pour cet événement.',
+              style: Theme.of(context).textTheme.bodyLarge,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
