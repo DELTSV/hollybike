e0b45d8ce1b42488b74b15d0943042ac59035e5c
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2024-06-18 15:46:06 +0200

    Fixed bad mapbox token

--- packages/infrastructure/project/backend/env-mapbox.tf
+++ packages/infrastructure/project/backend/env-mapbox.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "backend_mapbox_public_token" {
   name  = "/${lower(var.namespace)}/backend/${var.environment}/mapbox_public_token"
-  value = var.storage_s3_bucket_name
+  value = var.mapbox_public_token
   type  = "SecureString"
 }
 
