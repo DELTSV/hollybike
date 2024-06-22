0fa378f5365d683faa48ee71737636ca23526a10
MacaronFR <denisft77@gmail.com>
2024-06-22 17:42:20 +0200

    Better management of webSocket routes

--- packages/backend/src/main/kotlin/hollybike/api/routing/controller/WebSocketController.kt
+++ packages/backend/src/main/kotlin/hollybike/api/routing/controller/WebSocketController.kt
@@ -4,6 +4,8 @@ import hollybike.api.repository.User
 import hollybike.api.services.NotificationService
 import hollybike.api.types.websocket.Subscribe
 import hollybike.api.types.websocket.Subscribed
+import hollybike.api.types.websocket.Unsubscribe
+import hollybike.api.types.websocket.Unsubscribed
 import hollybike.api.utils.websocket.AuthVerifier
 import hollybike.api.utils.websocket.WebSocketRouter
 import hollybike.api.utils.websocket.webSocket
@@ -27,19 +29,27 @@ class WebSocketController(
 	}
 
 	private fun WebSocketRouter.notification() {
+		var user: User?
 		request("/notification") {
-			var user: User? = null
 			when(this.body) {
 				is Subscribe -> {
 					user = authVerifier.verify(this.body.token)
 					user?.let {
 						respond(Subscribed(true))
 						for( notif in notificationService.getUserChannel(it.id.value)) {
+							if(user == null){
+								break
+							}
 							respond(notif.data)
 						}
 					} ?: run {
 						respond(Subscribed(false))
 					}
+				}
+				is Unsubscribe -> {
+					println("Unsub")
+					respond(Unsubscribed(true))
+					user = null
 				}
 				else -> {
 					println("Unknown data")
