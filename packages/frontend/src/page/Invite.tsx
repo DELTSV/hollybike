7e6beb4210c0434e1fdf7913d81f7e9a55c7ea17
MacaronFR <denisft77@gmail.com>
2024-06-26 11:50:42 +0200

    Better ui

--- packages/frontend/src/page/Invite.tsx
+++ packages/frontend/src/page/Invite.tsx
@@ -1,9 +1,9 @@
 export function Invite() {
 	return (
-		<div className={"flex items-center gap-8 justify-center flex-col"}>
-			<h1 className={"text-4xl"}>Hollybike</h1>
+		<div className={"flex items-center gap-8 justify-center flex-col p-12"}>
+			<h1 className={"text-4xl md:text-9xl"}>Hollybike</h1>
 			<a
-				className={"w-1/2 flex justify-center"}
+				className={"w-full sm:w-3/4 lg:w-1/2 flex justify-center"}
 				href="https://play.google.com/store/apps/details?id=com.hollybike.hollybike&pcampaignid=pcampaignidMKT-
 				Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
 			>
