73ccb25fece1457592f554ea5ff33d1fd6e882f1
enzo soares <enzoayrsoares@gmail.com>
2024-07-13 22:59:52 +0200

    Styling add expense modal

--- packages/app/lib/shared/widgets/text_field/common_text_field.dart
+++ packages/app/lib/shared/widgets/text_field/common_text_field.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CommonTextField extends StatelessWidget {
   final TextEditingController? controller;
   final String title;
-  final String? Function(String?) validator;
+  final String? Function(String?)? validator;
   final bool obscureText;
   final IconButton? iconButton;
   final FocusNode? focusNode;
@@ -11,12 +12,14 @@ class CommonTextField extends StatelessWidget {
   final bool autofocus;
   final Iterable<String>? autofillHints;
   final TextInputType? textInputType;
+  final AutovalidateMode? autovalidateMode;
+  final List<TextInputFormatter>? inputFormatters;
 
   const CommonTextField({
     super.key,
     required this.controller,
     required this.title,
-    required this.validator,
+    this.validator,
     this.obscureText = false,
     this.iconButton,
     this.focusNode,
@@ -24,6 +27,8 @@ class CommonTextField extends StatelessWidget {
     this.autofocus = false,
     this.autofillHints,
     this.textInputType,
+    this.autovalidateMode,
+    this.inputFormatters,
   });
 
   @override
@@ -46,11 +51,12 @@ class CommonTextField extends StatelessWidget {
         validator: validator,
         obscureText: obscureText,
         decoration: decoration,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
+        autovalidateMode: autovalidateMode ?? AutovalidateMode.onUserInteraction,
         autofocus: autofocus,
         autofillHints: autofillHints,
         keyboardType: textInputType,
         focusNode: focusNode,
+        inputFormatters: inputFormatters,
         onEditingComplete: onEditingDone,
       ),
     );
