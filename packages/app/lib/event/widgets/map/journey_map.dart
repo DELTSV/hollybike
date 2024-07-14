f11436a7ac9beba16c3bbfb54cb42ee7cb4982e9
enzo soares <enzoayrsoares@gmail.com>
2024-07-14 19:11:08 +0200

    Fixing pin duplication

--- packages/app/lib/event/widgets/map/journey_map.dart
+++ packages/app/lib/event/widgets/map/journey_map.dart
@@ -3,11 +3,10 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hollybike/journey/service/journey_position_manager.dart';
 import 'package:hollybike/journey/type/minimal_journey.dart';
 import 'package:hollybike/positions/bloc/user_positions/user_positions_bloc.dart';
-import 'package:hollybike/positions/bloc/user_positions/user_positions_state.dart';
 import 'package:hollybike/shared/types/geojson.dart';
 import 'package:hollybike/shared/utils/waiter.dart';
 import 'package:hollybike/shared/websocket/recieve/websocket_receive_position.dart';
@@ -32,7 +31,6 @@ class JourneyMap extends StatefulWidget {
 }
 
 class _JourneyMapState extends State<JourneyMap> {
-  late final List<int> placeholderProfilePicture;
   Map<int, PointAnnotation> currentPositions = {};
   bool _mapLoading = true;
 
@@ -71,13 +69,6 @@ class _JourneyMapState extends State<JourneyMap> {
   @override
   void initState() {
     super.initState();
-    rootBundle
-        .load(
-      "assets/images/placeholder_map_pin.png",
-    )
-        .then((ressource) {
-      placeholderProfilePicture = ressource.buffer.asUint8List();
-    });
   }
 
   Future<String> _getGeoJsonData(String fileUrl) async {
@@ -155,28 +146,30 @@ class _JourneyMapState extends State<JourneyMap> {
           '[ "interpolate", ["linear"], ["zoom"], 15, 0, 15.05, ["get", "min_height"] ]',
         ),
         map.annotations.createPointAnnotationManager().then(
-          (pointManager) {
-            final userPositionsBloc =
-                BlocProvider.of<UserPositionsBloc>(context);
-            _updateMap(
-              pointManager,
-              userPositionsBloc.state,
+          (pointManager) async {
+            final journeyPositionManager = JourneyPositionManager(
+              pointManager: pointManager,
+              context: context,
             );
-            userPositionsBloc.stream.listen(
-              (state) {
-                _updateMap(
-                  pointManager,
-                  state,
-                );
+            final userPositionsBloc = BlocProvider.of<UserPositionsBloc>(
+              context,
+            );
 
-                _setCameraOptions(
-                  geoJsonRaw,
-                  state.userPositions,
-                  map,
-                  updateMode: true,
-                );
-              },
-            );
+            Timer(const Duration(seconds: 1), () {
+              journeyPositionManager.updatePositions(userPositionsBloc.state.userPositions);
+
+              userPositionsBloc.stream.listen(
+                    (state) {
+                  journeyPositionManager.updatePositions(state.userPositions);
+                  _setCameraOptions(
+                    geoJsonRaw,
+                    state.userPositions,
+                    map,
+                    updateMode: true,
+                  );
+                },
+              );
+            });
           },
         ),
       ]);
@@ -269,126 +262,5 @@ class _JourneyMapState extends State<JourneyMap> {
     }
 
     return cameraOptions;
-  }
-
-  void _updateMap(
-    PointAnnotationManager pointManager,
-    UserPositionsState userPositionsState,
-  ) async {
-    final missingPositions = <WebsocketReceivePosition>[];
-    final alreadyAddedPositions = <WebsocketReceivePosition>[];
-
-    for (final position in userPositionsState.userPositions) {
-      print(
-          "${position.userId} ${position.longitude}  ${position.longitude} $currentPositions");
-      if (currentPositions.containsKey(position.userId)) {
-        alreadyAddedPositions.add(position);
-      } else {
-        missingPositions.add(position);
-      }
-    }
-
-    for (final userId in currentPositions.keys) {
-      if (!userPositionsState.userPositions.any((p) => p.userId == userId)) {
-        final point = currentPositions[userId];
-
-        if (point != null) {
-          await pointManager.delete(point);
-        }
-      }
-    }
-
-    final addedPositions = (await Future.wait(
-      missingPositions
-          .map((position) => _addMapPosition(pointManager, position)),
-    ))
-        .whereType<MapEntry<int, PointAnnotation>>();
-
-    final updatedPositions = await Future.wait(
-      alreadyAddedPositions
-          .map((position) => _updateMapPosition(pointManager, position)),
-    );
-
-    setState(() {
-      currentPositions = Map.fromEntries(
-        [...addedPositions, ...updatedPositions],
-      );
-    });
-  }
-
-  Future<MapEntry<int, PointAnnotation>?> _addMapPosition(
-    PointAnnotationManager pointManager,
-    WebsocketReceivePosition missingPosition,
-  ) async {
-    final colorScheme = Theme.of(context).colorScheme;
-
-    final user = BlocProvider.of<UserPositionsBloc>(context)
-        .getPositionUser(missingPosition);
-    if (user is! UserLoadSuccessEvent) return null;
-
-    final profilePicture =
-        BlocProvider.of<UserPositionsBloc>(context).getUserPicture(user);
-
-    if (user.user.profilePicture != null &&
-        profilePicture is! UserPictureLoadSuccessEvent) return null;
-
-    final options = PointAnnotationOptions(
-      geometry: Point(
-        coordinates: Position(
-          missingPosition.longitude,
-          missingPosition.latitude,
-        ),
-      ),
-      image: (profilePicture is UserPictureLoadSuccessEvent
-          ? profilePicture.image
-          : placeholderProfilePicture) as Uint8List,
-      iconSize: profilePicture is UserPictureLoadSuccessEvent ? /*0.390625*/ 1 : 1,
-      iconAnchor: IconAnchor.BOTTOM,
-      textField:
-          '${user.user.username}\n${(missingPosition.speed * 3.6).round()} km/h',
-      textAnchor: TextAnchor.TOP,
-      textSize: 12,
-      textHaloWidth: 2,
-      textHaloColor: colorScheme.primary.value,
-      textColor: colorScheme.onPrimary.value,
-    );
-    final point = await pointManager.create(options);
-
-    return MapEntry(missingPosition.userId, point);
-  }
-
-  Future<MapEntry<int, PointAnnotation>> _updateMapPosition(
-    PointAnnotationManager pointManager,
-    WebsocketReceivePosition positionToUpdate,
-  ) async {
-    final point = currentPositions[positionToUpdate.userId];
-    if (point == null) {
-      throw Exception("Cannot find point for key $positionToUpdate");
-    }
-
-    point.geometry = Point(
-      coordinates: Position(
-        positionToUpdate.longitude,
-        positionToUpdate.latitude,
-      ),
-    );
-
-    final user = BlocProvider.of<UserPositionsBloc>(context)
-        .getPositionUser(positionToUpdate);
-    if (user is UserLoadSuccessEvent) {
-      final profilePicture =
-          BlocProvider.of<UserPositionsBloc>(context).getUserPicture(user);
-      point.image = (profilePicture is UserPictureLoadSuccessEvent
-          ? profilePicture.image
-          : placeholderProfilePicture) as Uint8List;
-      point.iconSize =
-          profilePicture is UserPictureLoadSuccessEvent ? /*0.390625*/ 1 : 1;
-
-      point.textField =
-          '${user.user.username}\n${(positionToUpdate.speed * 3.6).round()} km/h';
-    }
-
-    await pointManager.update(point);
-    return MapEntry(positionToUpdate.userId, point);
   }
 }
