f7f16ed4a754167d785feabc4c46c1b785b14efa
MacaronFR <denisft77@gmail.com>
2024-07-14 23:19:44 +0200

    Fix image not send by backend on premise

--- packages/backend/src/main/kotlin/hollybike/api/Front.kt
+++ packages/backend/src/main/kotlin/hollybike/api/Front.kt
@@ -3,6 +3,7 @@ package hollybike.api
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -10,11 +11,23 @@ fun Application.frontend() {
 	if (isOnPremise) {
 		routing {
 			get("/{...}") {
-				this::class.java.getResource("/front/index.html")?.readText()?.let {
-					call.respondText(it, ContentType.Text.Html)
+				if(call.request.uri.endsWith(".png")) {
+					this::class.java.getResource("/front${call.request.uri}")?.readBytes()?.let {
+						call.respondBytes(it, ContentType.Image.PNG)
+					}
+				}else if(call.request.uri.endsWith(".jpg")) {
+					this::class.java.getResource("/front${call.request.uri}")?.readBytes()?.let {
+						call.respondBytes(it, ContentType.Image.JPEG)
+					}
+				} else {
+					println("INDEX ${call.request.uri}")
+					this::class.java.getResource("/front/index.html")?.readText()?.let {
+						call.respondText(it, ContentType.Text.Html)
+					}
 				}
 			}
 			staticResources("/assets", "front/assets")
+			staticResources("/", "front")
 		}
 	}
 }
